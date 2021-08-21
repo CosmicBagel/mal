3478d2093a14fa4b34841657a517b0a1c8f7233b
Samuel Whiteley <sam@cosmicbagel.com>
2021-08-20 18:57:29 -0600

    step0 passes tests

--- impls/rust_bagel/step0_repl.rs
+++ impls/rust_bagel/step0_repl.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("fdjskfldsajkfdsajkfl");
+use std::io::{self, stdin, Write};
+
+fn main() -> io::Result<()> {
+    let mut buf = String::new();
+    loop {
+        let prompt = "user> ";
+        io::stdout().write_all(prompt.as_bytes())?;
+        io::stdout().flush()?;
+
+        stdin().read_line(&mut buf)?;
+        //READ - EVAL- PRINT
+        io::stdout().write_all(buf.as_bytes())?;
+        io::stdout().flush()?;
+        buf.clear();
+    }
 }
