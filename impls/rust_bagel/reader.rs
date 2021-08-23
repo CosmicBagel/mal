57a90a826e92380a00a3dee7526f3a95abfa1560
Samuel Whiteley <sam@cosmicbagel.com>
2021-08-22 22:03:54 -0600

    read_atom implemented, types required also implemented

--- impls/rust_bagel/reader.rs
+++ impls/rust_bagel/reader.rs
@@ -9,30 +9,24 @@ lazy_static! {
     static ref TOKEN_REGEX: regex::Regex = regex::Regex::new(REGEX_PATTERN).unwrap();
 }
 
-// type Tokens =
+type Tokens<'a> = Peekable<regex::CaptureMatches<'a, 'a>>;
 
 pub fn read_str(input_str: &str) -> Result<AST, regex::Error> {
     /*
        call tokenize and then create a new Reader object instance with the tokens. Then it
        will call read_form with the Reader instance.
     */
-    let tokens = tokenize(input_str);
-    for t in tokens {
-        println!("{:#?}", t);
-    }
-    read_from();
+    let mut tokens = tokenize(input_str);
+    let mal_type = read_from(&mut tokens);
+    println!("{}", mal_type);
     Ok(Vec::new())
 }
 
-pub fn tokenize(input_str: &str) -> Peekable<regex::CaptureMatches> {
-    /*
-        take a single string and return an array/list of all the tokens (strings) in it.
-        The following regular expression (PCRE) will match all mal tokens.
-    */
+pub fn tokenize(input_str: &str) -> Tokens {
     TOKEN_REGEX.captures_iter(input_str).peekable()
 }
 
-pub fn read_from() -> Vec<MalType> {
+pub fn read_from(mut tokens: &mut Tokens) -> MalType {
     /*
        peek at the first token in the Reader object and switch on the first character of that
        token. If the character is a left paren then read_list is called with the Reader object.
@@ -44,10 +38,17 @@ pub fn read_from() -> Vec<MalType> {
        list/array of other MalTypes. If your language is dynamically typed then you can likely
        just return a plain list/array of other mal types.
     */
-    Vec::new()
+    if let Some(t) = tokens.peek() {
+        match &t[0] {
+            "(" => read_list(&mut tokens),
+            _ => read_atom(&mut tokens),
+        }
+    } else {
+        MalType::Nil
+    }
 }
 
-pub fn read_list() -> Vec<MalType> {
+pub fn read_list(mut tokens: &mut Tokens) -> MalType {
     /*
         repeatedly call read_form with the Reader object until it encounters a ')' token
        (if it reach EOF before reading a ')' then that is an error). It accumulates the
@@ -56,10 +57,10 @@ pub fn read_list() -> Vec<MalType> {
        read_list repeatedly calls read_form rather than read_atom. This mutually recursive
        definition between read_list and read_form is what allows lists to contain lists.
     */
-    Vec::new()
+    MalType::List(Vec::new())
 }
 
-pub fn read_atom() -> MalType {
+pub fn read_atom(tokens: &mut Tokens) -> MalType {
     /*
         look at the contents of the token and return the appropriate scalar (simple/single)
         data type value. Initially, you can just implement numbers (integers) and symbols.
@@ -70,5 +71,45 @@ pub fn read_atom() -> MalType {
         are just an object that contains a single string name value (some languages have
         symbol types already).
     */
-    MalType::Number(0)
+    if let Some(cap) = tokens.next() {
+        let t = &cap[0];
+        if let Ok(int) = t.parse() {
+            MalType::Integer(int)
+        } else if let Ok(float) = t.parse() {
+            MalType::Float(float)
+        } else if let Ok(sym) = parse_symbol(t) {
+            sym
+        } else if t.to_lowercase() == "true" {
+            MalType::True
+        } else if t.to_lowercase() == "false" {
+            MalType::False
+        } else {
+            MalType::String(t.to_string())
+        }
+    } else {
+        MalType::Nil
+    }
+}
+
+fn parse_symbol(token_str: &str) -> Result<MalType, SymbolParseError> {
+    match token_str {
+        "*" => Ok(MalType::Symbol(Symbol::Asterisk)),
+        "-" => Ok(MalType::Symbol(Symbol::Minus)),
+        "+" => Ok(MalType::Symbol(Symbol::Plus)),
+        "/" => Ok(MalType::Symbol(Symbol::Divide)),
+        "=" => Ok(MalType::Symbol(Symbol::Equals)),
+        "!" => Ok(MalType::Symbol(Symbol::Exclamation)),
+        "&" => Ok(MalType::Symbol(Symbol::Ampersand)),
+        "%" => Ok(MalType::Symbol(Symbol::Modulus)),
+        ":" => Ok(MalType::Symbol(Symbol::Colon)),
+        ";" => Ok(MalType::Symbol(Symbol::SemiColon)),
+        r#"""# => Ok(MalType::Symbol(Symbol::DoubleQuote)),
+        "'" => Ok(MalType::Symbol(Symbol::SingleQuote)),
+        "?" => Ok(MalType::Symbol(Symbol::QuestionMark)),
+        "<" => Ok(MalType::Symbol(Symbol::LessThan)),
+        ">" => Ok(MalType::Symbol(Symbol::GreaterThan)),
+        "," => Ok(MalType::Symbol(Symbol::Comma)),
+        "." => Ok(MalType::Symbol(Symbol::Period)),
+        _ => Err(SymbolParseError::NotASymbol),
+    }
 }
--- impls/rust_bagel/types.rs
+++ impls/rust_bagel/types.rs
@@ -1,5 +1,98 @@
+use std::fmt;
+
+#[derive(Debug)]
 pub enum MalType {
-    Number(i32),
+    Integer(i32),
+    Float(f32),
+    String(String),
+    Symbol(Symbol),
+    True,
+    False,
+    Nil,
+    List(Vec<MalType>),
+}
+
+impl fmt::Display for MalType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MalType::Integer(v) => write!(f, "{}", v),
+            MalType::Float(v) => write!(f, "{}", v),
+            MalType::String(v) => write!(f, "{}", v),
+            MalType::Symbol(v) => write!(f, "{}", v),
+            MalType::True => write!(f, "True"),
+            MalType::False => write!(f, "False"),
+            MalType::Nil => write!(f, "Nil"),
+            MalType::List(v) => {
+                let mut out = String::new();
+                for e in v {
+                    out.push_str(&e.to_string());
+                }
+                write!(f, "{}", out)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Symbol {
+    Asterisk,
+    Plus,
+    Minus,
+    Divide,
+    Equals,
+    Ampersand,
+    Exclamation,
+    Modulus,
+    Colon,
+    SemiColon,
+    DoubleQuote,
+    SingleQuote,
+    QuestionMark,
+    LessThan,
+    GreaterThan,
+    Comma,
+    Period,
+    Unknown,
+}
+
+impl fmt::Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Symbol::Asterisk => write!(f, "*"),
+            Symbol::Plus => write!(f, "+"),
+            Symbol::Minus => write!(f, "-"),
+            Symbol::Divide => write!(f, "/"),
+            Symbol::Equals => write!(f, "="),
+            Symbol::Ampersand => write!(f, "&"),
+            Symbol::Exclamation => write!(f, "!"),
+            Symbol::Modulus => write!(f, "%"),
+            Symbol::Colon => write!(f, ":"),
+            Symbol::SemiColon => write!(f, ";"),
+            Symbol::DoubleQuote => write!(f, r#"""#),
+            Symbol::SingleQuote => write!(f, "'"),
+            Symbol::QuestionMark => write!(f, "?"),
+            Symbol::LessThan => write!(f, "<"),
+            Symbol::GreaterThan => write!(f, ">"),
+            Symbol::Comma => write!(f, ","),
+            Symbol::Period => write!(f, "."),
+            Symbol::Unknown => write!(f, "U N K N O W N"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum SymbolParseError {
+    NotASymbol,
+}
+
+impl std::error::Error for SymbolParseError {}
+
+impl fmt::Display for SymbolParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SymbolParseError::NotASymbol => write!(f, "Not a symbol"),
+        }
+    }
 }
 
 pub type AST = Vec<MalType>;
